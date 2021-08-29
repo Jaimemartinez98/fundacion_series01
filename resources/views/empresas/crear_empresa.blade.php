5784306c14a52e8f38d7f879d4693c9d6347c8c3
Jaimemartinez98 <82061295+Jaimemartinez98@users.noreply.github.com>
2021-08-29 10:52:58 -0500

    Clase 29 de agosto del 20210 v2

--- resources/views/empresas/crear_empresa.blade.php
+++ resources/views/empresas/crear_empresa.blade.php
@@ -1,7 +1,46 @@
 @extends('master')
 
+<link rel="stylesheet" href="{{ asset('/css/estilos_empresa.css') }}" />
+
+
 @section('content')
+
+    <form action="{{ route('empresas.store') }}" method="POST">
+        @csrf
+
+        <div class="row">
+
+            <div class="col-12">
+                <label for="nombre_empresa" class="form-label">Nombre Empresa</label>
+                <input type="text" class="form-control" id="nombre_empresa" name="nombre_empresa"
+                    placeholder="Introduzca el nombre de la empresa">
+            </div>
+
+            <div class="col-4">
+                <label for="celular_empresa" class="form-label">Celular</label>
+                <input type="text" class="form-control" id="celular_empresa" name="celular_empresa"
+                    placeholder="Introduzca el celular de la empresa">
+            </div>
+
+            <div class="col-4">
+                <label for="direccion_empresa" class="form-label">Dirección</label>
+                <input type="text" class="form-control" id="direccion_empresa" name="direccion_empresa"
+                    placeholder="Introduzca el dirección de la empresa">
+            </div>
+            <div class="col-4">
+                <label for="nit_empresa" class="form-label">NIT</label>
+                <input type="text" class="form-control" id="nit_empresa" name="nit_empresa"
+                    placeholder="Introduzca el NIT de la empresa">
+            </div>
+
+            <div class="col-12 estilo_center">
+                <button type="submit" class="btn btn-success estilo_boton">ENVIAR</button>
+            </div>
+
+        </div>
+
+
+    </form>
 
 
 @endsection
-
--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -6,7 +6,7 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
     </head>
 
-    <body>
+    <body style="padding: 10px 20px 20px 20px;">
 
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container-fluid">
@@ -17,7 +17,7 @@
               <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                   <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
                   </li>
                   <li class="nav-item">
                     <a class="nav-link" href="{{ route('empresas.index') }}">Listar Empresas</a>
